cc78736b8070e3f96ea28244da29958e1492255e
Shivanna <shivannadm6@gmail.com>
2024-02-18 14:21:24 +0530

    updated

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
