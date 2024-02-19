1824561ae49140c6eae3cafba09bbad5bf8a597c
Shivanna <shivannadm6@gmail.com>
2024-02-19 19:13:00 +0530

    updated

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("ecommerceapp.urls")),
-    path('authcart',include("authcart.urls"))
+    path('auth/',include("authcart.urls"))
 ]
