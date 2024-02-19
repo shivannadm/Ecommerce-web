d8ab5e82470609257303297165123cde226b7f70
Shivanna <shivannadm6@gmail.com>
2024-02-19 21:55:59 +0530

    updated

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def signup(request):
-    return render(request,"authentication/signup.html")
+    return render(request,"signup.html")
 
 def handlelogin(request):
-    return render(request,"authentication/login.html")
+    return render(request,"login.html")
 
 def handlelogout(request):
     return redirect('/auth/login')
