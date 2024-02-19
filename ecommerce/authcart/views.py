1a7d2ff97f675cd4bb745d3b99702014f1873c0d
Shivanna <shivannadm6@gmail.com>
2024-02-19 22:41:10 +0530

    updated

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def signup(request):
+    print("I'm running")
     return render(request,"signup.html")
 
 def handlelogin(request):
